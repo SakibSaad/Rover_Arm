329a260b0ea891a6e61d8bf3a5df6371fdec817e
AAMasud <amasud183040@bscse.uiu.ac.bd>
2024-04-03 08:03:36 +0600

    optimized scripts for server

--- stop_dashboard.sh
+++ stop_dashboard.sh
@@ -32,21 +32,3 @@ then
   kill -9 $ros_api_pid # -9 to kill the parent too | without this it dosen't work
   echo "killed ros_api_pid"
 fi
-
-
-# kill rover_latency_node
-ping_pid=$(ps aux |grep "python3 scripts/rover_latency_node" |awk '{for(i=1;i<=NF;i++) if ($i ~ /^[0-9]+$/) print $i}' |head -n1)
-if [[ -n "$ping_pid" ]]
-then
-  kill $ping_pid
-  echo "killed rover_latency_node"
-fi
-
-
-# kill speak_node
-speak_pid=$(ps aux |grep "python3 scripts/speak_node" |awk '{for(i=1;i<=NF;i++) if ($i ~ /^[0-9]+$/) print $i}' |head -n1)
-if [[ -n "$speak_pid" ]]
-then
-  kill $speak_pid
-  echo "killed speak_node"
-fi
